c5f4d498e4274cb0d330dbab56bb58b6dd94b5f7
Danny Fitzpatrick <hiddenpigeon@localhost.localdomain>
2014-03-29 10:26:30 +0000

    Initial commit

---
+++ booking_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookingApp::Application.config.secret_key_base = '47cef5ec15116d352b135379be88eb53fd2dfb07e00dad39fd63852ab126d9f2da9d952654c376bb826cff23e29dbc6fa5027471ec87f2177aeb4e9ae08a7d8f'
